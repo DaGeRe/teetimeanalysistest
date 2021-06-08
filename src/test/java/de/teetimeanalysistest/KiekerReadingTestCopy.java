a9bea3e9d9bee4134a3e52073a8833589f5a91c7
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-06-08 11:47:44 +0200

    Repeat copied reading

--- src/test/java/de/teetimeanalysistest/KiekerReadingTestCopy.java
+++ src/test/java/de/teetimeanalysistest/KiekerReadingTestCopy.java
@@ -20,9 +20,33 @@ public class KiekerReadingTestCopy {
    @Test(timeout = Constants.ONE_MINUTE_IN_MILLISECONDS)
    public void testReading() throws IOException {
       File inputFile = new File("src/test/resources/kieker-20210107-215803-8575705594504340-UTC--KIEKER-KoPeMe");
-      cleanup();
-      FileUtils.copyDirectory(inputFile, testFolder);
 
+      for (int i = 0; i < 15; i++) {
+         cleanup();
+         FileUtils.copyDirectory(inputFile, testFolder);
+
+         GetExecutionsStage resultStage = executeReading();
+
+         MatcherAssert.assertThat(resultStage.getSignatures(), IsCollectionContaining.hasItem("public new viewtest.TestMe$InnerClass.<init>(viewtest.TestMe)"));
+      }
+
+   }
+
+   @Test(timeout = Constants.ONE_MINUTE_IN_MILLISECONDS)
+   public void testReadingWithNewModifier() throws IOException {
+      File inputFile = new File("src/test/resources/kieker-20210321-103510-3402657433607-UTC--KIEKER-KoPeMe");
+
+      for (int i = 0; i < 15; i++) {
+         cleanup();
+         FileUtils.copyDirectory(inputFile, testFolder);
+
+         GetExecutionsStage resultStage = executeReading();
+
+         MatcherAssert.assertThat(resultStage.getSignatures(), IsCollectionContaining.hasItem("public new defaultpackage.NormalDependency.<init>()"));
+      }
+   }
+
+   private GetExecutionsStage executeReading() {
       KiekerReader app = new KiekerReader();
       GetExecutionsStage resultStage = app.exampleReader(testFolder);
 
@@ -30,33 +54,12 @@ public class KiekerReadingTestCopy {
       execution.executeBlocking();
 
       System.out.println(resultStage.getSignatures());
-
-      MatcherAssert.assertThat(resultStage.getSignatures(), IsCollectionContaining.hasItem("public new viewtest.TestMe$InnerClass.<init>(viewtest.TestMe)"));
-
-      cleanup();
+      return resultStage;
    }
 
    private void cleanup() throws IOException {
       if (testFolder.exists()) {
          FileUtils.deleteDirectory(testFolder);
       }
-   }
-
-   @Test(timeout = Constants.ONE_MINUTE_IN_MILLISECONDS)
-   public void testReadingWithNewModifier() throws IOException {
-      File inputFile = new File("src/test/resources/kieker-20210321-103510-3402657433607-UTC--KIEKER-KoPeMe");
-      cleanup();
-      FileUtils.copyDirectory(inputFile, testFolder);
-
-      KiekerReader app = new KiekerReader();
-      GetExecutionsStage resultStage = app.exampleReader(inputFile);
-
-      Execution execution = new Execution(app);
-      execution.executeBlocking();
-
-      System.out.println(resultStage.getSignatures());
-      MatcherAssert.assertThat(resultStage.getSignatures(), IsCollectionContaining.hasItem("public new defaultpackage.NormalDependency.<init>()"));
-
-      cleanup();
    }
 }
